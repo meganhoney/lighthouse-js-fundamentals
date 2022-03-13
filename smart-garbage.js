803bdb30c5340a417f4ce297f97e4c5048a356fc
Megan Honey <mjthoney@gmail.com>
2022-03-13 23:19:54 +0000

    Final javascript exercisees with Objects

---
+++ smart-garbage.js
@@ -0,0 +1,31 @@
+function smartGarbage(trash, bins); {
+ if(trash === bins) {
+
+ }
+}
+
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
+
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+}
+
+console.log(pizza);
+
+/*const food = {
+  types: 'only pizza'
+}
+
+console.log(food['types']);
+
+const car = {
+  make: 'Honda',
+  model: 'Accord',
+  year: 2020
+}
+
+const keys = Object.keys(car);
+
+console.log(keys);*/
